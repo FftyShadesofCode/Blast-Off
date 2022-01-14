947058aaec4aa135d29e0ffe439872805e4d1520
FftyShadesofCode <philiprmcdavid@gmail.com>
2022-01-14 18:40:23 -0500

    getting things accomplished

---
+++ server/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '569515127448-8u31gqdqnv0usjfl5nk5ljcmbgdqhbar.apps.googleusercontent.com',
+    googleClientSecret: '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',
+    mongoURI: 'mongodb+srv://fiftyshadesofcode:Bensboy2005@blast-off.5b8rn.mongodb.net/Blast-Off?retryWrites=true&w=majority',
+    cookieKey: 'shksjhfksfsfjslkfjleroirqofnklsf55afhauijfa2298uif'
+}
