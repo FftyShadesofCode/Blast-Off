ceb45b44eea92a2e943721f5413357ac29a84299
FftyShadesofCode <philiprmcdavid@gmail.com>
2022-01-14 19:17:40 -0500

    creating secret keys

---
+++ server/config/dev.js
@@ -0,0 +1,7 @@
+// dev.js don't commit this
+module.exports = {
+    googleClientID: '885060699601-ib62vqgc64eheiikjcj16juen1ut95l8.apps.googleusercontent.com',
+    googleClientSecret: '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',
+    mongoURI: 'mongodb+srv://fiftyshadesofcode:Bensboy2005@blast-off.5b8rn.mongodb.net/Blast-Off?retryWrites=true&w=majority',
+    cookieKey: 'shksjhfksfsfjslkfjleroirqofnklsf55afhauijfa2298uif'
+}
