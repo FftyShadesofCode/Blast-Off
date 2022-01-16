ca22716631d9ab6bc3e9d1da509c557521961d5f
FftyShadesofCode <philiprmcdavid@gmail.com>
2022-01-16 10:53:56 -0500

    Making head-way

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,6 +1,6 @@
 // dev.js don't commit this
 module.exports = {
-    googleClientID: '885060699601-ib62vqgc64eheiikjcj16juen1ut95l8.apps.googleusercontent.com',
+    googleClientID: '569515127448-8u31gqdqnv0usjfl5nk5ljcmbgdqhbar.apps.googleusercontent.com',
     googleClientSecret: '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',
     mongoURI: 'mongodb+srv://fiftyshadesofcode:Bensboy2005@blast-off.5b8rn.mongodb.net/Blast-Off?retryWrites=true&w=majority',
     cookieKey: 'shksjhfksfsfjslkfjleroirqofnklsf55afhauijfa2298uif',
